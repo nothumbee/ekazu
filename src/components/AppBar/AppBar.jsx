e73cdebdf502ed68dfbf9b24ecd03b72a5c1ba17
David Šupík <developersupik@gmail.com>
2019-04-17 17:27:38 +0200

    layout-created

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React,  {useState} from 'react';
 // import LoadPatient from '../Load/LoadPatient';
 import { Link, withRouter } from 'react-router-dom';
-
+import { Menu } from 'antd';
 import * as ROUTES from '../../constants/routes';
+import 'antd/dist/antd.css';
+
 
 const AppBar = () => {
+
+  const [current, setCurrent] = useState('LANDING');
+
+  const handleClick = (e) => {
+    console.log('click ', e);
+    setCurrent(
+     e.key,
+    );
+  }
+
+
   return (
-    <ul>
-      <li>
+
+    <Menu onClick={handleClick} selectedKeys={[current]}  mode="horizontal" theme="dark" >
+      <Menu.Item key="LANDING">
         <Link to={ROUTES.LANDING}>Landing</Link>
-      </li>
-      <li>
+      </Menu.Item>
+      <Menu.Item key="ADMIN">
         <Link to={ROUTES.ADMIN}>ADMIN</Link>
-      </li>
-      <li>
+      </Menu.Item>
+      <Menu.Item key="STUDENT">
         <Link to={ROUTES.STUDENT}>STUDENT</Link>
-      </li>
-      {/* <LoadPatient /> */}
-    </ul>
+      </Menu.Item>
+  </Menu>
+
+
   );
 };
 
