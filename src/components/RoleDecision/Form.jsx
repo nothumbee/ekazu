749f832ba05fea7b69b39bde93d2a1594ea5b60e
David Šupík <developersupik@gmail.com>
2019-04-27 13:49:56 +0200

    adj

--- src/components/RoleDecision/Form.jsx
+++ src/components/RoleDecision/Form.jsx
@@ -4,18 +4,26 @@ import { Link } from "react-router-dom";
 import "./Form.css";
 
 import { STUDENT, ADMIN } from "../../constants/routes";
+import { ReactComponent as StudentIllustration } from "./student.svg";
+import { ReactComponent as TeacherIllustration } from "./teacher.svg";
 
 const RoleDecisionForm = () => {
   return (
-    <>
+    <div className="RoleDecisionForm">
       <div className="title">Jste učitel nebo student?</div>
-      <div className="item">
-        <Link to={STUDENT}>Student</Link>
-      </div>
-      <div className="item">
-        <Link to={ADMIN}>Učitel</Link>
-      </div>
-    </>
+      <Link to={STUDENT}>
+        <div className="item">
+          <StudentIllustration />
+          <div className="text">Student</div>
+        </div>
+      </Link>
+      <Link to={ADMIN}>
+        <div className="item">
+          <TeacherIllustration />
+          <div className="text">Učitel</div>
+        </div>
+      </Link>
+    </div>
   );
 };
 
