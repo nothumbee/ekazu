00ac20e4f4076cf50c96236211b1f5459db5be20
David Šupík <developersupik@gmail.com>
2019-04-27 14:30:29 +0200

    jk

--- src/components/RoleDecision/Form.jsx
+++ src/components/RoleDecision/Form.jsx
@@ -9,21 +9,23 @@ import { ReactComponent as TeacherIllustration } from "./teacher.svg";
 
 const RoleDecisionForm = () => {
   return (
-    <div className="RoleDecisionForm">
-      <div className="title">Jste učitel nebo student?</div>
-      <Link to={STUDENT}>
-        <div className="item">
-          <StudentIllustration />
-          <div className="text">Student</div>
-        </div>
-      </Link>
-      <Link to={ADMIN}>
-        <div className="item">
-          <TeacherIllustration />
-          <div className="text">Učitel</div>
-        </div>
-      </Link>
-    </div>
+    <>
+      <div className="RoleDecisionForm">
+        <div className="title">Jste učitel nebo student?</div>
+        <Link to={STUDENT}>
+          <div className="item">
+            <StudentIllustration />
+            <div className="text">Student</div>
+          </div>
+        </Link>
+        <Link to={ADMIN}>
+          <div className="item">
+            <TeacherIllustration />
+            <div className="text">Učitel</div>
+          </div>
+        </Link>
+      </div>
+    </>
   );
 };
 
